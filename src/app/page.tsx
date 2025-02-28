d0e471a027f5d7f321df4f05736dd885e096b111
Matheus Carvalho <henriquematheus.c@gmail.com>
2025-02-28 17:44:35 -0300

    Create button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <div>
-      <h2 className="card bg-">Hello world</h2>
+      <h2 className="card icon-brand">Hello world</h2>
+      <Button size="md" variant="primary">
+        Button
+      </Button>
     </div>
   );
 }
---
+++ src/components/button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { tv, VariantProps } from "tailwind-variants";
+import { Slot } from "@radix-ui/react-slot";
+
+const buttonLero = tv({
+  base: "inline-flex gap-2 items-center justify-center transition-colors duration-300 cursor-pointer rounded-full text-sm",
+  variants: {
+    size: {
+      md: "px-6 py-3",
+      sm: "py-2 px-3",
+    },
+    variant: {
+      primary: "bg-brand-primary text-on-brand hover:bg-brand-primary-hover disabled:bg-disabled disabled:border-disabled disabled:border disabled:text-on-disabled",
+      outline: "bg-default border border-default text-default hover:bg-brand-secondary hover:text-brand-secondary disabled:bg-disabled disabled:border-disabled disabled:border disabled:text-on-disabled",
+      subtle: "text-default hover:text-brand-secondary disabled:text-on-disabled",
+    },
+    defaultVariants: {
+      variant: "md",
+      size: "primary",
+    },
+  },
+});
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonLero> & {
+    asChild?: boolean;
+  };
+
+function Button({ className, variant, size, asChild = false, ...props }: ButtonProps, ref: React.Ref<HTMLButtonElement>) {
+  const classes = buttonLero({ size, variant, className });
+  const Comp = asChild ? Slot : "button";
+
+  return <Comp className={classes} ref={ref} {...props} />;
+}
+
+const ForwardedButton = React.forwardRef(Button);
+
+export { ForwardedButton as Button };
