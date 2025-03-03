2e4c7a839fe0e533b84c731952d3a7dd7add5398
Matheus Carvalho <henriquematheus.c@gmail.com>
2025-03-03 12:30:57 -0300

    Fix topbar on mobile

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -4,13 +4,13 @@ import { Countdown } from "./countdown";
 export function Topbar() {
   return (
     <div className="bg-brand-primary text-on-brand py-4">
-      <div className="container flex items-center justify-between">
-        <p className="text-left text-sm">
+      <div className="container flex flex-col items-center justify-between gap-4 lg:flex-row">
+        <p className="text-center text-sm lg:text-left">
           Gotex Show 12ª edição <br />
           Evento presencial
         </p>
         <Countdown />
-        <p className="text-right text-sm">
+        <p className="text-center text-sm lg:text-right">
           05 a 07 de agosto de 2025 <br />
           Distrito Anhembi, Expo 2 - São Paulo
         </p>
