0d68787ad548e68dddebc626b8872ac755c0798c
JuniorDevas <elson.almeida.junior@gmail.com>
2025-03-05 17:07:14 -0300

    Creat section 'organizadore-e-agencia-oficial'

--- src/app/(frontend)/seja-um-expositor/page.tsx
+++ src/app/(frontend)/seja-um-expositor/page.tsx
@@ -159,6 +159,25 @@ export default function Home() {
           <a href="">Quero expor</a>
         </div>
       </section>
+
+      <section className="py-24">
+        <div className="container">
+          <div className="text-center grid md:grid-cols-2 gap-y-10">
+            <div>
+              <a href="https://www.8com.com.br/" title="Link para o site da organizadora OitoCom" target="_blank" rel="noopener noreferrer">
+                <span>Organizador</span>
+                <img className="m-auto" src="/images/seja-um-expositor/logo-oitocom.webp" alt="Logo da organizadora OitoCom" width={133} height={133} loading="lazy" />
+              </a>
+            </div>
+            <div>
+              <a href="https://jogajunto.co/" title="Link para o site da agência oficial JogaJunto" target="_blank" rel="noopener noreferrer">
+                <span>Agência oficial</span>
+                <img className="m-auto" src="/images/seja-um-expositor/logo-jogajunto.svg" alt="Logo da agência oficial JogaJunto" width={133} height={133} loading="lazy" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
